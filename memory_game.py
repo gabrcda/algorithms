3dbd28efa6cbe413c2f4a35c13893cc3dc122960
unknown <gabri22kocebrac@gmail.com>
2024-03-29 17:33:25 -0300

    refact the populate board function

--- memory_game.py
+++ memory_game.py
@@ -3,28 +3,26 @@ import random
 
 def make_boards():
     game_board = ["#" for _ in range(24)]
-    board_empty = game_board.copy()
+    empty_board = game_board.copy()
 
-    return game_board, board_empty
+    return game_board, empty_board
 
 def populate_board_game(board):
     symbols = ["x", "o", "#", "+", "-", "@"]
     cells = [n for n in range(24)]
-    unsorted_cells = []
-    
+    count = 0
+
     for i in range(24):
         number = random.sample(cells, 1)
-        unsorted_cells.append(number[0])
-        board[unsorted_cells[i]] = symbols[int(i/6)]
+        board[number[0]] = symbols[count]
         cells.remove(number[0])
+        count = 0 if count >= 5 else count + 1
         
-    print(unsorted_cells)
-    print(board)
-
+    return board
 
 
 if __name__ == "__main__":
-    game_board, board_empty = make_boards()
+    game_board, empty_board = make_boards()
     # print(game_board)
     # print(board_empty)
-    populate_board_game(game_board)
+    game_board = populate_board_game(game_board)
